377b5dbf06d82915000288735ae600e8db515e51
Andrea Carraro <me@andreacarraro.it>
2025-09-08 10:18:37 +0200

    refactor: make JSONSchema type readonly

--- src/types.ts
+++ src/types.ts
@@ -5,12 +5,13 @@ import type {
 } from 'type-fest';
 
 // https://github.com/ThomasAribart/json-schema-to-ts/blob/12767c1eab895159c01f5e6898d8e5e711ff9d1c/src/definitions/jsonSchema.ts
-export type JSONSchema = {
+export type JSONSchema = Readonly<{
   type: string;
   required?: readonly string[];
   properties?: Readonly<Record<string, unknown>>;
   patternProperties?: Readonly<Record<string, unknown>>;
-};
+  additionalProperties?: boolean;
+}>;
 
 export type JSONSchemaObject = OverrideProperties<
   JSONSchema,
