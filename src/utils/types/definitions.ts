d2320813ae1f8bb925607dd1845d4cab2dbe35d3
Andrea Carraro <me@andreacarraro.it>
2025-10-31 23:44:16 +0100

    feat: make input JSONschema types stricter

--- src/utils/types/definitions.ts
+++ src/utils/types/definitions.ts
@@ -1,17 +1,36 @@
 import type { MergeRecords, Simplify } from './index';
 
+export type JSONSchemaType =
+  | 'string'
+  | 'number'
+  | 'integer'
+  | 'boolean'
+  | 'object'
+  | 'array'
+  | 'null';
+
 // https://github.com/ThomasAribart/json-schema-to-ts/blob/12767c1eab895159c01f5e6898d8e5e711ff9d1c/src/definitions/jsonSchema.ts
-export type JSONSchema = Readonly<{
-  type: string;
-  required?: readonly string[];
-  properties?: Readonly<Record<string, unknown>>;
-  patternProperties?: Readonly<Record<string, unknown>>;
-  additionalProperties?: boolean;
-}>;
+export type JSONSchema =
+  | boolean
+  | Readonly<{
+      type: JSONSchemaType;
+      required?: readonly string[];
+      properties?: Readonly<Record<string, unknown>>;
+      patternProperties?: Readonly<Record<string, unknown>>;
+
+      additionalProperties?: JSONSchema;
+      unevaluatedProperties?: JSONSchema;
+
+      // Combiners
+      allOf?: readonly JSONSchema[];
+      anyOf?: readonly JSONSchema[];
+      oneOf?: readonly JSONSchema[];
+      not?: JSONSchema;
+    }>;
 
 export type JSONSchemaObject = Simplify<
   MergeRecords<
-    JSONSchema,
+    Exclude<JSONSchema, boolean>,
     {
       type: 'object';
       properties: Readonly<Record<string, unknown>>;
